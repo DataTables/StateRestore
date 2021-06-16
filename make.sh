b2e961b367887446217f6931f63d1364d3e681fd
SandyDatatables <sandy@datatables.net>
2021-06-16 14:01:48 +0000

    Add make.sh file

---
+++ make.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+DT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../.."
+if [ "$1" = "debug" ]; then
+    DEBUG="debug"
+else
+    OUT_DIR=$1
+    DEBUG=$2
+fi
+
+# If not run from DataTables build script, redirect to there
+if [ -z "$DT_BUILD" ]; then
+    cd $DT_DIR/build
+    ./make.sh extension StateRestore $DEBUG
+    cd -
+    exit
+fi
+
+# Change into script's own dir
+cd $(dirname $0)
+
+DT_SRC=$(dirname $(dirname $(pwd)))
+DT_BUILT="${DT_SRC}/built/DataTables"
+. $DT_SRC/build/include.sh
+
+# Copy CSS
+rsync -r css $OUT_DIR
+css_frameworks searchPanes $OUT_DIR/css
+
+if [ ! -d "node_modules" ]; then
+    npm install
+fi
+
+# Copy images
+#rsync -r images $OUT_DIR
+
+node_modules/typescript/bin/tsc
+
+# Copy JS
+HEADER="$(head -n 3 src/index.ts)"
+
+rsync -r src/*.js $OUT_DIR/js
+js_frameworks stateRestore $OUT_DIR/js
+
+OUT=$OUT_DIR ./node_modules/rollup/dist/bin/rollup \
+    --banner "$HEADER" \
+    --config rollup.config.js
+
+rm \
+    src/*.js \
+    # src/*.d.ts
+
+mv src/*.d.ts types/
+
+# Copy Types
+if [ -d $OUT_DIR/types ]; then
+	rm -r $OUT_DIR/types		
+fi
+mkdir $OUT_DIR/types
+
+if [ -d types/ ]; then
+	cp types/* $OUT_DIR/types
+else
+	if [ -f types.d.ts ]; then
+		cp types.d.ts $OUT_DIR/types
+	fi
+fi
+
+js_compress $OUT_DIR/js/dataTables.stateRestore.js
+
+# Copy and build examples
+rsync -r examples $OUT_DIR
+examples_process $OUT_DIR/examples
+
+# Readme and license
+cp Readme.md $OUT_DIR
+cp License.txt $OUT_DIR
+
