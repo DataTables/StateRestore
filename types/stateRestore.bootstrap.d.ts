0b56665987411a6c4e9a58e242e805ea0d229c9e
SandyDatatables <sandy@datatables.net>
2021-07-23 13:44:13 +0000

    update git ignore to remove auto types files

--- types/stateRestore.bootstrap.d.ts
+++
@@ -1,7 +0,0 @@
-/*! Bootstrap integration for DataTables' SearchPanes
- * ©2016 SpryMedia Ltd - datatables.net/license
- */
-declare let define: {
-    (stringValue: any, Function: any): any;
-    amd: string;
-};
