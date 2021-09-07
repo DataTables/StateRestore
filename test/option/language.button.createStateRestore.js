3664396980ae37147eb53cff1da015350b72035b
Colin Marks <colin0117@yahoo.com>
2021-09-07 11:52:12 +0100

    TEST first test

---
+++ test/option/language.button.createStateRestore.js
@@ -0,0 +1,37 @@
+describe('stateRestore - options - language.buttons.stateRestore.count', function () {
+	let table;
+
+	dt.libs({
+		js: ['jquery', 'datatables', 'buttons', 'staterestore'],
+		css: ['datatables', 'buttons', 'staterestore']
+	});
+
+	describe('Functional tests', function () {
+		dt.html('basic');
+		it('Check defaults', function () {
+			table = $('#example').DataTable({
+				dom: 'Blfrtip',
+				buttons: ['createStateRestore']
+			});
+
+			expect($('.dt-button').text()).toBe('Create State');
+		});
+
+		dt.html('basic');
+		it('Change text', function () {
+			$.fn.dataTable.ext.buttons.createStateRestore.text = "Create new state";
+
+			table = $('#example').DataTable({
+				dom: 'Blfrtip',
+				buttons: ['createStateRestore'],
+				// language: {
+				// 	buttons: {
+				// 		createStateRestore: 'unit test'
+				// 	}
+				// }
+			});
+
+			expect($('.dt-button').text()).toBe('unit test');
+		});
+	});
+});
