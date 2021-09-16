72dc36fd567270a4be0d8122120dd6fdfc56bdfe
Colin Marks <colin@datatables.net>
2021-09-16 11:19:37 +0000

    TESt tests added for the new rror language error messages

---
+++ test/option/language.stateRestore.emptyError.js
@@ -0,0 +1,65 @@
+describe('stateRestore - options - language.stateRestore.emptyError', function () {
+	let table;
+
+	dt.libs({
+		js: ['jquery', 'datatables', 'buttons', 'staterestore'],
+		css: ['datatables', 'buttons', 'staterestore']
+	});
+
+	describe('Functional tests', function () {
+		dt.html('basic');
+		it('Create a state', function () {
+			$.fx.off = true; // disables lightbox animation
+
+			table = $('#example').DataTable({
+				dom: 'Blfrtip',
+				buttons: ['createState', 'savedStates']
+			});
+
+			$('.dt-button:eq(0)').click();
+			$('.dt-button:eq(1)').click();
+
+			expect($('.dt-btn-split-wrapper .dt-button').length).toBe(2);
+		});
+		it('Check defaults', function () {
+			$('.dt-btn-split-wrapper .dt-button:eq(1)').click();
+			$('.dt-btn-split-drop-button:eq(2)').click();
+			$('input.dtsr-input').val('');
+			$('.dtsr-confirmation-button').click();
+
+			expect($('.dtsr-modal-error').text()).toBe('Name cannot be empty.');
+		});
+
+		dt.html('basic');
+		it('Change text', function () {
+			table = $('#example').DataTable({
+				dom: 'Blfrtip',
+				buttons: ['createState', 'savedStates'],
+				language: {
+					stateRestore: {
+						emptyError: 'unit test'
+					}
+				}
+			});
+
+			$('.dt-button:eq(1)').click();
+			$('.dt-btn-split-wrapper .dt-button:eq(1)').click();
+			$('.dt-btn-split-drop-button:eq(2)').click();
+			$('input.dtsr-input').val('');
+			$('.dtsr-confirmation-button').click();
+
+			expect($('.dtsr-modal-error').text()).toBe('unit test');
+		});
+	});
+
+	describe('Tidy up', function () {
+		dt.html('basic');
+		it('Delete all state', function () {
+			table = $('#example').DataTable({
+				dom: 'Blfrtip',
+				buttons: ['createState', 'savedStates']
+			});
+			table.stateRestore.states().delete(true);
+		});
+	});
+});
