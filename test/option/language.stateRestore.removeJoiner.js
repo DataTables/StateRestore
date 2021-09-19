8db2e6af6f7a12c3e90e2f046fadbabd5cc94917
Colin Marks <colin@datatables.net>
2021-09-19 08:14:59 +0000

    TEST new test for language.stateRestore.removeJoiner

---
+++ test/option/language.stateRestore.removeJoiner.js
@@ -0,0 +1,59 @@
+describe('stateRestore - options - language.stateRestore.removeJoiner', function () {
+	let table;
+
+	dt.libs({
+		js: ['jquery', 'datatables', 'buttons', 'staterestore'],
+		css: ['datatables', 'buttons', 'staterestore']
+	});
+
+	describe('Functional tests', function () {
+		dt.html('basic');
+		it('Create two states', function () {
+			$.fx.off = true; // disables lightbox animation
+
+			table = $('#example').DataTable({
+				dom: 'Blfrtip',
+				buttons: ['createState', 'savedStates']
+			});
+
+			$('.dt-button:eq(0)').click();
+			$('.dt-button:eq(0)').click();
+			$('.dt-button:eq(1)').click();
+
+			expect($('.dt-btn-split-wrapper .dt-button').length).toBe(4);
+		});
+		it('Check defaults', function () {
+			table.stateRestore.states().remove();
+
+			expect($('.dtsr-confirmation-text').text()).toBe('Are you sure you want to remove State 1 and State 2?');
+		});
+
+		dt.html('basic');
+		it('Change text', function () {
+			table = $('#example').DataTable({
+				dom: 'Blfrtip',
+				buttons: ['createState', 'savedStates'],
+				language: {
+					stateRestore: {
+						removeJoiner: ' unit test '
+					}
+				}
+			});
+
+			table.stateRestore.states().remove();
+
+			expect($('.dtsr-confirmation-text').text()).toBe('Are you sure you want to remove State 1 unit test State 2?');
+		});
+	});
+
+	describe('Tidy up', function () {
+		dt.html('basic');
+		it('Remove all state', function () {
+			table = $('#example').DataTable({
+				dom: 'Blfrtip',
+				buttons: ['createState', 'savedStates']
+			});
+			table.stateRestore.states().remove(true);
+		});
+	});
+});
