dec35ce72e02b83c0c80bedd81968289c4cabb6e
Allan Jardine <allan@datatables.net>
2021-12-03 13:14:21 +0000

    Release 1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-/*! StateRestore 1.0.0
+/*! StateRestore 1.0.1
  * 2019-2020 SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     StateRestore
  * @description StateRestore extension for DataTables
- * @version     1.0.0
+ * @version     1.0.1
  * @author      SpryMedia Ltd (www.sprymedia.co.uk)
  * @copyright   Copyright 2019-2020 SpryMedia Ltd.
  *
