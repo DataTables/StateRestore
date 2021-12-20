451b43c033d3e5e13367826995a27042be56010e
SandyDatatables <sandy@datatables.net>
2021-12-20 15:45:33 +0000

    Fix issues with rollup

--- rollup.config.js
+++ rollup.config.js
@@ -8,45 +8,5 @@ export default [
 			format: 'iife'
 		},
 		plugins: [resolve()]
-	},
-	{
-		input: 'src/stateRestore.bootstrap.js',
-		output: {
-			file: process.env.OUT + '/js/stateRestore.bootstrap.js',
-			format: 'iife'
-		},
-		plugins: [resolve()]
-	},
-	{
-		input: 'src/stateRestore.bootstrap4.js',
-		output: {
-			file: process.env.OUT + '/js/stateRestore.bootstrap4.js',
-			format: 'iife'
-		},
-		plugins: [resolve()]
-	},
-	{
-		input: 'src/stateRestore.dataTables.js',
-		output: {
-			file: process.env.OUT + '/js/stateRestore.dataTables.js',
-			format: 'iife'
-		},
-		plugins: [resolve()]
-	},
-	{
-		input: 'src/stateRestore.foundation.js',
-		output: {
-			file: process.env.OUT + '/js/stateRestore.foundation.js',
-			format: 'iife'
-		},
-		plugins: [resolve()]
-	},
-	{
-		input: 'src/stateRestore.semanticui.js',
-		output: {
-			file: process.env.OUT + '/js/stateRestore.semanticui.js',
-			format: 'iife'
-		},
-		plugins: [resolve()]
 	}
 ];
