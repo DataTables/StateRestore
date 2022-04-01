7cd719713dccb05f911d81fbd9637ca82e93bdf7
Colin Marks <colin0117@yahoo.com>
2022-04-01 12:15:01 +0100

    DD-2515 existing tests for RemoveAllStates were, well, rubbish, so refactored and extended

--- test/buttons/removeAllStates.js
+++ test/buttons/removeAllStates.js
@@ -8,7 +8,7 @@ describe('stateRestore - buttons - removeAllStates', function () {
 
 	describe('Functional tests', function () {
 		dt.html('basic');
-		it('Create a three states', function () {
+		it('Create three states', function () {
 			$.fx.off = true; // disables lightbox animation
 
 			table = $('#example').DataTable({
@@ -24,13 +24,60 @@ describe('stateRestore - buttons - removeAllStates', function () {
 
 			expect($('.dt-button-collection .dt-button').length).toBe(6);
 		});
-		it('No states', function () {
-			$('.dt-buttons button:nth-child(2)').click();
-			$('.dt-buttons button:nth-child(3)').click();
+		it('... Remove all button is active', function () {
+			// Close the collection
+			$('.buttons-collection').click();
+
+			expect($('.dtsr-removeAllStates').hasClass('disabled')).toBe(false);
+		});		
+		it('... Click remove all button', function () {
+			$('.dtsr-removeAllStates').click();
+
+			expect($('.dtsr-removeAllStates').hasClass('disabled')).toBe(true);
+		});
+		it('... and all states are removed', function () {
 			$('.dt-buttons button:nth-child(2)').click();
 
 			expect($('.dt-button-collection .dt-button').length).toBe(0);
+		});
 
+		dt.html('basic');
+		it('Alternative UI: Create three states', function () {
+			$.fx.off = true; // disables lightbox animation
+
+			table = $('#example').DataTable({
+				dom: 'Blfrtip',
+				buttons: [{
+					extend: 'savedStates',
+					buttons: [
+						'createState',
+						'removeAllStates'
+					]
+				}]
+			});
+
+			$('.buttons-collection').click();
+			$('.dt-button-collection .dt-button').click();
+			$('.dt-button-collection .dt-button').click();
+			$('.dt-button-collection .dt-button').click();
+
+			expect($('.dt-button-collection .dt-button').length).toBe(8);
+			expect($('.dtsr-emptyStates').text()).toBe('');
+
+		});
+		it('... Remove all button is active', function () {
+			expect($('.dtsr-removeAllStates').hasClass('disabled')).toBe(false);
+		});
+		it('... Click remove all button', function () {
+			$('.dtsr-removeAllStates').click();
+			$('.buttons-collection').click();
+
+			expect($('.dtsr-removeAllStates').hasClass('disabled')).toBe(true);
+		});
+		it('... and all states are removed', function () {
+			$('.dt-buttons button:nth-child(2)').click();
+
+			expect($('.dtsr-emptyStates').text()).toBe('No saved states');
 		});
 	});
 
