9272f0a5e26e4a21dab2a5d02a21ee486856a80a
Allan Jardine <allan@datatables.net>
2022-10-26 14:40:02 +0000

    New: Typescript definitions rewritten to fully interoperate with DataTables own Typescript definitions.

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,94 +1,144 @@
+// Type definitions for DataTables StateRestore
+//
+// Project: https://datatables.net/extensions/StateRestore/, https://datatables.net
+
 /// <reference types="jquery" />
-/// <reference types="datatables.net"/>
 
-
+import DataTables, {Api} from 'datatables.net';
 import * as stateRestoreCollectionType from './StateRestoreCollection';
 import * as stateRestoreType from './StateRestore';
 
-declare namespace DataTables {
+export default DataTables;
 
-	interface Settings {
+type DeepPartial<T> = T extends object ? {
+    [P in keyof T]?: DeepPartial<T[P]>;
+} : T;
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * DataTables' types integration
+ */
+declare module 'datatables.net' {
+	interface Config {
 		/**
 		 * StateRestore extension options
 		 */
-		stateRestore?: boolean | string[] | stateRestoreCollectionType.IDefaults | stateRestoreCollectionType.IDefaults[];
+		stateRestore?: boolean | string[] | ConfigStateRestore | ConfigStateRestore[];
 	}
 
-	interface LanguageSettings {
-		stateRestore?: {};
+	interface ConfigLanguage {
+		/**
+		 * StateRestore language options
+		 */
+		stateRestore?: ConfigStateRestoreLanguage;
 	}
 
 	interface Api<T> {
 		/**
 		 * StateRestore API Methods
 		 */
-		stateRestore: StateRestoreGlobalApi;
+		stateRestore: ApiStateRestore<T>;
 	}
 
-	interface StateRestoreGlobalApi {
+	interface ApiStatic {
 		/**
-		 * Creates a new state, adding it to the collection.
-		 *
-		 * @param identifier The identifier that is to be used for the new state
-		 *
-		 * @returns DatatTables Api for chaining
+		 * StateRestore class
 		 */
-		addState(identifier: string): void | Api<any>;
+		StateRestore: {
+			/**
+			 * Create a new StateRestore instance for the target DataTable
+			 */
+			new (dt: Api<any>, settings: string[] | ConfigStateRestore | ConfigStateRestore[]);
 
-		/**
-		 * Retrieves a state from the collection.
-		 *
-		 * @param identifier The identifier of the state that is to be retrieved.
-		 *
-		 * @returns StateRestore instance, or further api methods.
-		 */
-		state(identifier: string): stateRestoreType.default | null | StateRestoreSubApi;
+			/**
+			 * StateRestore version
+			 */
+			version: string;
 
-		/**
-		 * Retrieves all of the states from the collection.
-		 *
-		 * @returns An array of the StateRestore instances,
-		 * or further api methods that are applicable to multiple states.
-		 */
-		states(): stateRestoreType.default[] | StateRestoreMultiSubApi;
+			/**
+			 * Default configuration values
+			 */
+			defaults: ConfigStateRestore;
+		}
 	}
+}
 
-	interface StateRestoreSubApi {
-		/**
-		 * Removes the state previously identified in the call to `state()`.
-		 *
-		 * @returns Datatables Api for chaining.
-		 */
-		remove(): Api<any>;
 
-		/**
-		 * Loads the state previously identified in the call to `state()` into the table.
-		 *
-		 * @returns Datatables Api for chaining.
-		 */
-		load(): Api<any>;
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Options
+ */
+interface ConfigStateRestore extends Partial<stateRestoreCollectionType.IDefaults> {}
 
-		/**
-		 * Renames the state previously identified in the call to `state()`.
-		 *
-		 * @returns Datatables Api for chaining.
-		 */
-		rename(): Api<any>;
+interface ConfigStateRestoreLanguage extends DeepPartial<stateRestoreCollectionType.II18n> {}
 
-		/**
-		 * Saves the state previously identified in the call to `state()`.
-		 *
-		 * @returns Datatables Api for chaining.
-		 */
-		save(): Api<any>;
-	}
 
-	interface StateRestoreMultiSubApi {
-		/**
-		 * Removes all of the states that were previously identified in the call to `states()`.
-		 *
-		 * @returns Datatables Api for chaining.
-		 */
-		remove(): Api<any>;
-	}
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * API
+ */
+
+interface ApiStateRestore<T> {
+	/**
+	 * Creates a new state, adding it to the collection.
+	 *
+	 * @param identifier The identifier that is to be used for the new state
+	 *
+	 * @returns DatatTables Api for chaining
+	 */
+	addState(identifier: string): void | Api<T>;
+
+	/**
+	 * Retrieves a state from the collection.
+	 *
+	 * @param identifier The identifier of the state that is to be retrieved.
+	 *
+	 * @returns StateRestore instance, or further api methods.
+	 */
+	state(identifier: string): stateRestoreType.default | null | StateRestoreSubApi<T>;
+
+	/**
+	 * Retrieves all of the states from the collection.
+	 *
+	 * @returns An array of the StateRestore instances,
+	 * or further api methods that are applicable to multiple states.
+	 */
+	states(): stateRestoreType.default[] | StateRestoreMultiSubApi<T>;
+}
+
+interface StateRestoreSubApi<T> extends Api<T> {
+	/**
+	 * Removes the state previously identified in the call to `state()`.
+	 *
+	 * @returns Datatables Api for chaining.
+	 */
+	remove(): Api<T>;
+
+	/**
+	 * Loads the state previously identified in the call to `state()` into the table.
+	 *
+	 * @returns Datatables Api for chaining.
+	 */
+	load(): Api<T>;
+
+	/**
+	 * Renames the state previously identified in the call to `state()`.
+	 *
+	 * @returns Datatables Api for chaining.
+	 */
+	rename(): Api<T>;
+
+	/**
+	 * Saves the state previously identified in the call to `state()`.
+	 *
+	 * @returns Datatables Api for chaining.
+	 */
+	save(): Api<T>;
+}
+
+interface StateRestoreMultiSubApi<T> extends Api<T> {
+	/**
+	 * Removes all of the states that were previously identified in the call to `states()`.
+	 *
+	 * @returns Datatables Api for chaining.
+	 */
+	remove(): Api<T>;
 }
