215e8a4693aecff89cf289ba294e1576a4a7faec
Allan Jardine <allan@datatables.net>
2025-10-10 08:53:48 +0000

    Update version

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-/*! StateRestore 1.4.2
+/*! StateRestore 1.4.3
  * © SpryMedia Ltd - datatables.net/license
  */
 
 /**
  * @summary     StateRestore
  * @description StateRestore extension for DataTables
- * @version     1.4.2
+ * @version     1.4.3
  * @author      SpryMedia Ltd
  * @contact     datatables.net
  * @copyright   Copyright SpryMedia Ltd.
